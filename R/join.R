89a99606e7068138376a3a91e21c31d9fa20aa6a
overdodactyl <patjohns1216@gmail.com>
2018-10-16 19:45:01 -0600

    transition to dplyr and add timezones in join()

--- R/join.R
+++ R/join.R
@@ -3,7 +3,7 @@
 #' @param obj a NHD object with a COMID attribute
 #' @return a simple features (sf) object
 #' @export
-#' @author Mike Johnson
+#' @author Mike Johnson and Pat Johnson
 
 join = function(obj = NULL){
 
@@ -15,15 +15,33 @@ join = function(obj = NULL){
 
   if(is.null(ids)){stop("Input object doesn't contain COMID values")}
 
-  retro = nwmRetro::nwm_averages
+  # Define seasons
+  seasons <- list(
+    annual = month.abb,
+    fall   = c("Sep", "Oct", "Nov"),
+    spring = c("Mar", "Apr", "May"),
+    summer = c("Jun", "Jul", "Aug"),
+    winter = c("Dec", "Jan", "Feb")
+  )
 
-  retro = retro[retro$COMID %in% ids,]
+  # Scale and filter data
+  retro <- nwm_averages %>%
+    mutate_at(vars(one_of(month.abb)), funs(. / 10)) %>%
+    filter(COMID %in% ids)
 
-  retro$annual = rowMeans(retro[,c(2:12)])
-  retro$fall   = rowMeans(retro[,c(10,11,12)])
-  retro$spring = rowMeans(retro[,c(4,5,6)])
-  retro$summer = rowMeans(retro[,c(7,8,9)])
-  retro$winter = rowMeans(retro[,c(13,2,3)])
+  # Add summary statistics
+  for (season in names(seasons)) {
+    months = eval(parse(text = paste0('seasons$', season)))
+    retro <- retro %>%
+      mutate(!!season := rowMeans(select(., one_of(months)), na.rm = TRUE)) %>%
+      mutate_at(vars(season), round, 2)
+  }
+
+  # Add timezone name and timezone offset
+  retro <- retro %>%
+    mutate(timezone = timezones[tz_index, 1]) %>%
+    mutate(offset = timezones[tz_index, 2]) %>%
+    select(-tz_index)
 
   obj.new  = merge(obj, retro, by.x = "comid", by.y = 'COMID', all.x = TRUE)
 
--- R/nwmRetro.R
+++ R/nwmRetro.R
@@ -7,6 +7,7 @@
 #' @docType package
 #' @name nwmRetro
 #'
+#' @importFrom dplyr mutate_at one_of mutate select vars funs
 #' @importFrom sf st_write st_zm read_sf as_Spatial st_as_sf
 
 NULL
