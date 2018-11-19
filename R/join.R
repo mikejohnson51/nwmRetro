05cbfa44d66db5f80b2a866e47fa689f1f3016fb
overdodactyl <patjohns1216@gmail.com>
2018-11-19 16:56:34 -0700

    Reverting to the state of the project at 9afa339b26c

--- R/join.R
+++ R/join.R
@@ -3,11 +3,7 @@
 #' @param obj a NHD object with a COMID attribute
 #' @return a simple features (sf) object
 #' @export
-<<<<<<< HEAD
-#' @author Mike Johnson
-=======
 #' @author Mike Johnson and Pat Johnson
->>>>>>> cabb7542fda948e253be880263e733cc398e4e85
 
 join = function(obj = NULL){
 
@@ -19,17 +15,6 @@ join = function(obj = NULL){
 
   if(is.null(ids)){stop("Input object doesn't contain COMID values")}
 
-<<<<<<< HEAD
-  retro = nwmRetro::nwm_averages
-
-  retro = retro[retro$COMID %in% ids,]
-
-  retro$annual = rowMeans(retro[,c(2:12)])
-  retro$fall   = rowMeans(retro[,c(10,11,12)])
-  retro$spring = rowMeans(retro[,c(4,5,6)])
-  retro$summer = rowMeans(retro[,c(7,8,9)])
-  retro$winter = rowMeans(retro[,c(13,2,3)])
-=======
   # Define seasons
   seasons <- list(
     annual = month.abb,
@@ -48,7 +33,7 @@ join = function(obj = NULL){
   for (season in names(seasons)) {
     months = eval(parse(text = paste0('seasons$', season)))
     retro <- retro %>%
-      mutate(!!season := rowSums(select(., one_of(months)), na.rm = TRUE)) %>%
+      mutate(!!season := rowMeans(select(., one_of(months)), na.rm = TRUE)) %>%
       mutate_at(vars(season), round, 2)
   }
 
@@ -57,7 +42,6 @@ join = function(obj = NULL){
     mutate(timezone = timezones[tz_index, 1]) %>%
     mutate(offset = timezones[tz_index, 2]) %>%
     select(-tz_index)
->>>>>>> cabb7542fda948e253be880263e733cc398e4e85
 
   obj.new  = merge(obj, retro, by.x = "comid", by.y = 'COMID', all.x = TRUE)
 
--- data-raw/nwm_averages.R
+++ data-raw/nwm_averages.R
@@ -15,12 +15,6 @@ for (file in fileList) {
     inner_join(fst::read.fst(paste0("./data-raw/",file)), by = "COMID")
 }
 
-<<<<<<< HEAD
-# Define timezones
-timezones <- sort(unique(nwm_retro$tz))
-
-=======
->>>>>>> cabb7542fda948e253be880263e733cc398e4e85
 # Get offset for a specific timezone region
 t_offset <- function(tz) {
   res = c()
@@ -31,12 +25,6 @@ t_offset <- function(tz) {
   return(as.numeric(res))
 }
 
-<<<<<<< HEAD
-nwm_averages <- nwm_retro %>%
-  mutate_at(vars(one_of(month.abb)), round, 1) %>%
-  mutate_at(vars(one_of(month.abb)), funs(. * 10)) %>%
-  mutate(tz_index = match(tz,timezones)) %>%
-=======
 # Define timezones
 timezone_names <- sort(unique(nwm_retro$tz))
 tz_index <- t_offset(timezone_names)
@@ -47,14 +35,9 @@ devtools::use_data(timezones)
 nwm_averages <- nwm_retro %>%
   mutate_at(vars(one_of(month.abb)), funs(round(.,1) * 10)) %>%
   mutate(tz_index = match(tz,timezone_names)) %>%
->>>>>>> cabb7542fda948e253be880263e733cc398e4e85
   select(-lat, -long, -tz) %>%
   mutate_all(funs(as.integer(.)))
 
 
 
-<<<<<<< HEAD
-devtools::use_data(nwm_averages, compress = "xz")
-=======
 devtools::use_data(nwm_averages, compress = "xz", internal = TRUE)
->>>>>>> cabb7542fda948e253be880263e733cc398e4e85
